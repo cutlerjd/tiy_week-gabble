2f440a4e84f51f92b7258c97a09c09e4e39b231c
Jase Cutler <jasecutler@gmail.com>
2017-08-13 16:35:01 -0700

    Added in validations

--- routes/messageBoardRoute.js
+++ routes/messageBoardRoute.js
@@ -6,40 +6,47 @@ const expressValidator = require('express-validator')
 
 router.use(expressValidator())
 
-router.get("/", function(req,res,next){
-  Post.getAllPosts(req.user.id,function(results){
-    let posts = {posts:results}
+router.get("/", function (req, res, next) {
+  Post.getAllPosts(req.user.id, function (results) {
+    let posts = { posts: results }
     // console.dir(posts, {depth:null})
-    res.render("messagesHome",posts)
+    res.render("messagesHome", posts)
   })
 })
 
-router.get("/submit", function(req,res,next){
+router.get("/submit", function (req, res, next) {
   res.render("createPost")
 })
-router.post("/submit", function(req,res,next){
-  Post.createPost(req.body.id,req.body.text,function(success, result){
-    if(success){
+
+router.post("/submit", function (req, res, next) {
+  req.checkBody('text', "Length is too long!").isLength(0, 140)
+  let validationErrors = req.validationErrors()
+  if (!validationErrors) {
+    Post.createPost(req.body.id, req.body.text, function (success, result) {
+      if (success) {
+        res.redirect("/messages")
+      } else {
+        res.send("EEEEEERRRRROOORR")
+      }
+    })
+  } else {
+    res.send("Too many characters!")
+  }
+})
+router.get("/like/:id", function (req, res, next) {
+  Like.attachLike(req.user.id, req.params.id, function (success, result) {
+    if (success) {
       res.redirect("/messages")
     } else {
-      res.send("EEEEEERRRRROOORR")
-    }
-  })
-})
-router.get("/like/:id", function(req,res,next){
-  Like.attachLike(req.user.id,req.params.id,function(success,result){
-    if(success){
-      res.redirect("/messages")
-    }else {
       res.send("ERROR attaching like")
     }
   })
 })
-router.get("/delete/:id", function(req,res,next){
-  Post.deletePost(req.params.id,function(success,result){
-    if(success){
+router.get("/delete/:id", function (req, res, next) {
+  Post.deletePost(req.params.id, function (success, result) {
+    if (success) {
       res.redirect("/messages")
-    }else{
+    } else {
       res.send("Error")
     }
   })
